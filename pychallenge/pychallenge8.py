010984f42102f68cb7bc331e88e6a0ab05c4184e
crazyAxe <529076144@qq.com>
2016-06-22 11:09:29 +0800

    modified pychallenge8

---
+++ pychallenge/pychallenge8.py
@@ -0,0 +1,11 @@
+# 'BZh91AY' 实际上是bzip2算法的特征，相应的，'PK/x03/x04' 是zip算法的特征，而'/x25/xD5/b/b '则是gzip算法的特征
+
+import bz2
+name = b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+passwd = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+
+un = bz2.decompress(name)
+pd = bz2.decompress(passwd)
+print("%s, %s " % (un, pd))
+
+
